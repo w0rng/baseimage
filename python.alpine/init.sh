d2d6111696279758d159cc99741cba7eae67bb35
w0rng <work@w0rng.ru>
2024-06-17 08:00:31 +0700

    fix env dir and init app file

--- python.alpine/init.sh
+++ python.alpine/init.sh
@@ -17,18 +17,20 @@ if [[ ${uid} -eq 0 ]]; then
     else
         echo "custom APP_UID not defined, using default uid=1001"
     fi
-    chown -R app:app /srv /home/app
+    chown -R app:app /app /home/app /venv
 fi
 
 
-if [[ -f "/srv/init.sh" ]]; then
-    echo "execute /srv/init.sh"
-    chmod +x /srv/init.sh
-    /srv/init.sh
+if [[ -f "/app/init.sh" ]]; then
+    echo "execute /app/init.sh"
+    chmod +x /app/init.sh
+    /app/init.sh
     if [[ "$?" -ne "0" ]]; then
-      echo "/srv/init.sh failed"
+      echo "/app/init.sh failed"
       exit 1
     fi
+else
+    echo "/app/init.sh not found, starting without it"
 fi
 
 echo execute "$@"
